53653f84c8b159f4b69b4a247eb68a37af130d0b
jgomez881 <jgomez88@eafit.edu.co>
2018-03-21 20:39:03 -0500

    Add project files.

---
+++ PlanApp1/PlanApp1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,71 @@
+﻿namespace PlanApp1.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+    public class LoginViewModel
+	{
+
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+        #endregion
+
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+        }
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an password.",
+                    "Accept");
+                return;
+            }
+
+        }
+        #endregion
+    }
+}
---
+++ PlanApp1/PlanApp1/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace PlanApp1.ViewModels
+{
+	public class MainViewModel
+	{
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+    }
+}
