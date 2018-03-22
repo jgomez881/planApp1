3959d1d4a80cffe9249bb7a4dc2a028dfbdb8108
jgomez881 <jgomez88@eafit.edu.co>
2018-03-22 01:17:37 -0500

    Añadiendo BaseViewModel

--- PlanApp1/PlanApp1/ViewModels/LoginViewModel.cs
+++ PlanApp1/PlanApp1/ViewModels/LoginViewModel.cs
@@ -1,11 +1,16 @@
 ﻿namespace PlanApp1.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
-    using System;
     using System.Windows.Input;
     using Xamarin.Forms;
-    public class LoginViewModel
-	{
+    /* Hereda de la BaseViewModel*/
+    public class LoginViewModel : BaseViewModel
+    {
+        #region  Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
 
         #region Properties
         public string Email
@@ -17,8 +22,14 @@
 
         public string Password
         {
-            get;
-            set;
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
         }
 
         public bool IsRemembered
@@ -26,12 +37,19 @@
             get;
             set;
         }
+
+        public bool IsEnabled {
+            /*Refresco la aplicacion en tiempo de ejecucion */
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
         #endregion
 
         #region Constructors
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
 
@@ -43,6 +61,7 @@
                 return new RelayCommand(Login);
             }
         }
+
 
         private async void Login()
         {
@@ -63,6 +82,31 @@
                     "Accept");
                 return;
             }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jzuluaga55@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Email or password incorrect.",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            await Application.Current.MainPage.DisplayAlert(
+                   "ok",
+                   "coronamos.",
+                   "Accept");
+            return;
+
 
         }
         #endregion
