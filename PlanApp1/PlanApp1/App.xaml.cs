9597b1b3714486e9acad7cfa2cb6a890ebc590ed
slassog <slassog@eafit.edu.co>
2018-04-28 17:24:00 -0500

    Conexión con la bd funcional pero comentada

--- PlanApp1/PlanApp1/App.xaml.cs
+++ PlanApp1/PlanApp1/App.xaml.cs
@@ -2,15 +2,37 @@
 {
     using Views;
     using Xamarin.Forms;
-
+    using System;
+    using System.Data;
+    using System.Text;
+    using MySql.Data;
+    using MySql.Data.MySqlClient;
     public partial class App : Application
 	{
         #region Constructors
         public App()
         {
             InitializeComponent();
+            /* MySqlConnection conn;
+            string myConnectionString;
 
+            myConnectionString = "server=10.131.137.177;uid=user1;" +
+                "port=3306;pwd=eafit.2016;database=PlanApp";
+
+            Console.WriteLine("Connecting to MySQL");
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+
+                DataTable table = conn.GetSchema("Usuario");
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }*/
             this.MainPage = new NavigationPage(new LoginPage());
+
         }
         #endregion
 
