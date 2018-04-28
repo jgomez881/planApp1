13e2da03941a364ae695a08e8b43c04ae589f78c
slassog <slassog@eafit.edu.co>
2018-04-28 00:42:37 -0500

    Resolved problems

---
+++ planApp1/PlanApp1/PlanApp1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,154 @@
+﻿namespace PlanApp1.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using Views;
+    using Xamarin.Forms;
+    using Services;
+    /* Hereda de la BaseViewModel*/
+    public class LoginViewModel : BaseViewModel
+    {
+        /*aqui ponemos a refrescar los campos*/
+        #region  Attributes
+        private string password;
+        private string email;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
+        #region Services
+        DialogService dialogService;
+        #endregion
+
+        #region Properties
+        public string Email
+        {
+            /*Refresco la View y me la deja en blanco*/
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
+        }
+
+
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled {
+            /*Refresco la aplicacion en tiempo de ejecucion */
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            dialogService = new DialogService();
+            this.IsRemembered = true;
+            this.IsEnabled = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RegisterNewUserCommand
+        {
+            get
+            {
+                return new RelayCommand(RegisterNewUser);
+            }
+        }
+
+        async void RegisterNewUser()
+        {
+            MainViewModel.GetInstance().NewCustomer = new NewCustomerViewModel();
+            /*await navigationService.NavigateOnLogin("NewCustomerView");*/
+            await Application.Current.MainPage.Navigation.PushAsync(new NewCustomerPage());
+        }
+
+        public ICommand LoginCommand
+        {
+            get
+            {
+                return new RelayCommand(Login);
+            }
+
+        }
+
+
+        private async void Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+
+                await dialogService.ShowMessage("Error",
+                    "Debes ingresar un correo electrónico");
+                #region OtraForma
+                /*await Application.Current.MainPage.DisplayAlert(
+                "Error",
+                "You must enter an Email",
+                "Accept");*/
+                #endregion
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+
+                await dialogService.ShowMessage("Error",
+                    "Debes ingresar una contraseña");
+                #region OtraForma
+                /* await Application.Current.MainPage.DisplayAlert(
+                     "Error",
+                     "You must enter an password.",
+                     "Accept");*/
+                #endregion
+
+                return;
+            }
+
+            this.IsRunning = true;
+            this.IsEnabled = false;
+
+            if (this.Email != "jgomez88@gmail.com" || this.Password != "1234")
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await dialogService.ShowMessage("Error",
+                    "Email o contraseña incorrecta.");
+                #region otra forma
+                   await Application.Current.MainPage.DisplayAlert(
+                   "Error",
+                   "Email or password incorrect.",
+                   "Accept");
+                #endregion
+                this.Password = string.Empty;
+                return;
+            }
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            this.Email = string.Empty;
+            this.Password = string.Empty;
+
+            MainViewModel.GetInstance().Plans= new PlansViewModel();
+
+            await Application.Current.MainPage.Navigation.PushAsync(new PlansPage());
+
+        }
+        #endregion
+    }
+}
---
+++ planApp1/PlanApp1/PlanApp1/ViewModels/PlansViewModel.cs
@@ -0,0 +1,209 @@
+﻿namespace PlanApp1.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using Services;
+    using System.Windows.Input;
+
+    /*logica buscador*/
+    public class PlansViewModel : BaseViewModel
+	{
+
+
+        #region Services
+        DialogService dialogService;
+        #endregion
+
+        #region Attributes
+        bool isRefreshing;
+        string filter;
+        #endregion
+
+        #region Properties
+        public string Filter
+        {
+           get { return this.filter; }
+           set { SetValue(ref this.filter, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+        #endregion
+      
+        #region Constructors
+        public PlansViewModel()
+        {
+            instance = this;
+            dialogService = new DialogService();
+        }
+        #endregion
+       
+       #region Sigleton
+       static PlansViewModel instance;
+
+       public static PlansViewModel GetInstance()
+       {
+           if (instance == null)
+           {
+               return new PlansViewModel();
+           }
+
+           return instance;
+       }
+        #endregion
+
+        #region Comentado
+        /*
+        #region Methods
+        public void Add(Category category)
+        {
+            IsRefreshing = true;
+            categories.Add(category);
+            Categories = new ObservableCollection<Category>(
+                categories.OrderBy(c => c.Description));
+            IsRefreshing = false;
+        }
+
+        public void Update(Category category)
+        {
+            IsRefreshing = true;
+            var oldCategory = categories
+                .Where(c => c.CategoryId == category.CategoryId)
+                .FirstOrDefault();
+            oldCategory = category;
+            Categories = new ObservableCollection<Category>(
+                categories.OrderBy(c => c.Description));
+            IsRefreshing = false;
+        }
+
+        public async Task Delete(Category category)
+        {
+            IsRefreshing = true;
+
+            var connection = await apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                IsRefreshing = false;
+                await dialogService.ShowMessage("Error", connection.Message);
+                return;
+            }
+
+            var mainViewModel = MainViewModel.GetInstance();
+
+            var response = await apiService.Delete(
+                "http://productszuluapi.azurewebsites.net",
+                "/api",
+                "/Categories",
+                mainViewModel.Token.TokenType,
+                mainViewModel.Token.AccessToken,
+                category);
+
+            if (!response.IsSuccess)
+            {
+                IsRefreshing = false;
+                await dialogService.ShowMessage(
+                    "Error",
+                    response.Message);
+                return;
+            }
+
+            categories.Remove(category);
+            Categories = new ObservableCollection<Category>(
+                categories.OrderBy(c => c.Description));
+
+            IsRefreshing = false;
+        }
+
+        async void LoadCategories()
+        {
+            IsRefreshing = true;
+            var connection = await apiService.CheckConnection();
+            if (!connection.IsSuccess)
+            {
+                categories = dataService.Get<Category>(true);
+                if (categories.Count == 0)
+                {
+                    await dialogService.ShowMessage(
+                        "Error",
+                        "Dear user, YAPE");
+                    return;
+                }
+            }
+            else
+            {
+                var mainViewModel = MainViewModel.GetInstance();
+
+                var urlAPI = Application.Current.Resources["URLAPI"].ToString();
+
+                var response = await apiService.GetList<Category>(
+                    urlAPI,
+                    "/api",
+                    "/Categories",
+                    mainViewModel.Token.TokenType,
+                    mainViewModel.Token.AccessToken);
+
+                if (!response.IsSuccess)
+                {
+                    await dialogService.ShowMessage(
+                        "Error",
+                        response.Message);
+                    return;
+                }
+
+                categories = (List<Category>)response.Result;
+                SaveCategoriesOnDB();
+            }
+
+            Search();
+            IsRefreshing = false;
+        }
+
+        void SaveCategoriesOnDB()
+        {
+            dataService.DeleteAll<Category>();
+            foreach (var category in categories)
+            {
+                dataService.Insert(category);
+                dataService.Save(category.Products);
+            }
+        }
+        #endregion
+    */
+
+        #endregion
+        
+        #region Commands
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            }
+        }
+
+
+        /*ya con esto a medida que buscamos nos van apareciando sugerencias*/
+        void Search()
+        {
+            IsRefreshing = true;
+
+            if (string.IsNullOrEmpty(Filter))
+            {
+            }
+            else
+            {
+            }
+
+            IsRefreshing = false;
+        }
+
+        /*public ICommand RefreshCommand
+        {
+
+        }*/
+        #endregion
+    }
+}
+
